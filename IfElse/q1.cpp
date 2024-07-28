1c525d2828744d018812dd4e4f9de9c5c862a54e
Diluc99 <jhashivam53741@gmail.com>
2024-07-29 01:26:29 +0530

    adding initial files

---
+++ IfElse/q1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    //Write a program to print the value if it is even an divisible by 3
+    int num;
+    cin>>num;
+    if( (num%2==0) && (num%3==0) ) 
+    {cout<<num;};
+
+
+
+    return 0;
+}
---
+++ IfElse/q2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //Write a program to print the value if it is divisible by 3 or 5
+    int num;
+    cin>>num;
+    if ( (num%3==0) || (num%5==0) )
+    {cout<<num;} ;
+
+
+    return 0;
+}
