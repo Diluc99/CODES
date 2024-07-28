42edf1f03e8e279827ee2207bb3d9636fa1b1dc8
Diluc99 <jhashivam53741@gmail.com>
2024-07-28 12:19:10 +0530

    Add files via upload

---
+++ learning basics/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"Enter the no. : "<<;
+    cin>>num;
+    if (num%2=0)
+    cout<<num<<" is an even no. "<<endl;
+    else cout<<num<<"is a odd no. ";
+    return 0;
+    }
