ef34fd6b4ea060a567ab0e6c81cfed8f1a6f947f
Diluc99 <jhashivam53741@gmail.com>
2024-07-31 23:49:49 +0530

    adding statement for input and output

--- LOOPS/whileLoopBasic.cpp
+++ LOOPS/whileLoopBasic.cpp
@@ -4,13 +4,14 @@ int main()
 {
     //write a program to print the sum of first n natural numbers
     int n,a=1,sum=0;  // a is the loop variable
+    cout<<"Enter the value of n to get the sum of first n natural no.s : ";
     cin>>n;
     while (a<=n)  //condition
      {
        sum+=a;
         a++;  //updating loop variable
     }
-    cout<<sum;
+    cout<<"The sum of first " <<n << " natural no. is : " <<sum;
     
      return 0;
 }
