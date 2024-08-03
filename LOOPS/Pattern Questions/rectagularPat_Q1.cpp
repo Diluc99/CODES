ada614b52cc7230dbc439d1c26461a6b595d0270
Diluc99 <jhashivam53741@gmail.com>
2024-08-03 23:19:17 +0530

    Writing code

--- LOOPS/Pattern Questions/rectagularPat_Q1.cpp
+++ LOOPS/Pattern Questions/rectagularPat_Q1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{   
+    // Write a program to print a n*m rectangle of stars. m and n are no. of rows and column respectively. take user input for n and m.
+     int m,n;
+     cout<<"Enter no. of rows: ";
+     cin>>m;
+     cout<<"Enter no. of columns: ";
+     cin>>n;
 
+     for (int i=1;i<=m;i++)
+     {
+        for (int j=1;j<=n;j++)
+    {
+    cout<<"*";
+
+    }
+    cout<<endl;
+     }
+
+        return 0;
+}
